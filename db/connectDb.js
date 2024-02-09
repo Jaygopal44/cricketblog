aa8a3a1c8a88fa1f4b903bbac2da0b75812942bc
Jaygopal44 <76877611+Jaygopal44@users.noreply.github.com>
2024-02-09 13:37:37 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const live_url = "mongodb+srv://Jaygopal0075:bdHkCYlC3OtbfMP5@cluster0.02dbbxb.mongodb.net/BlogWeb?retryWrites=true&w=majority"
+// const live_url = "mongodb+srv://Jaygopal0075:bdHkCYlC3OtbfMP5@cluster0.02dbbxb.mongodb.net/BlogWeb?retryWrites=true&w=majority"
 
-// const live_url = "mongodb+srv://Jaygopal003:GjMqrNmcquI3iAcn@cluster0.hoey0x3.mongodb.net/BlogWeb?retryWrites=true&w=majority"
+const live_url = "mongodb+srv://Jaygopal003:MiWeU1S1iV0QnK4f@cluster0.hoey0x3.mongodb.net/BlogWeb?retryWrites=true&w=majority"
 // const local_url = mongoose.connect("mongodb://localhost:27017/exp_project")
 const connectDB=()=>{
     
